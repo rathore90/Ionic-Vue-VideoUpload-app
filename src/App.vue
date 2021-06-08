2878198da051843bf1c0b5099d0ce875df79f523
Pardeep <pardeep@sweetlegs.ca>
2021-06-08 07:41:52 -0700

    video upload initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Vue Video Capture</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-button @click="takeVideo">Take Video</ion-button>
+    </ion-content>
+  </ion-app>
 </template>
-
+<style src='@ionic/core/css/core.css'></style>
+<style src='@ionic/core/css/ionic.bundle.css'></style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { VideoCapturePlus } from "@ionic-native/video-capture-plus";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  methods: {
+    takeVideo: async function () {
+      try {
+        let options = { limit: 1, duration: 30 };
+        let video = await VideoCapturePlus.captureVideo(options);
+        console.log(video[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Ionic from '@ionic/vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Ionic)
 
 new Vue({
   render: h => h(App),
